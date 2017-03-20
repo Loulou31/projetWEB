e9c68f687d6c5a371a9684b637f3d6c3f8a48a40
Louise Nicolas <louise.nicolas@ensimag.grenoble-inp.fr>
2017-03-20 16:59:59 +0100

    passage en MVC

---
+++ projetWEB/src/main/java/controleur/Controleur.java
@@ -0,0 +1,80 @@
+
+package controleur;
+
+import dao.DAOException;
+import java.io.*;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.sql.DataSource;
+
+/**
+ * Le contrôleur de l'application.
+ */
+@WebServlet(name = "Controleur", urlPatterns = {"/controleur"})
+public class Controleur extends HttpServlet {
+
+    @Resource(name = "jdbc/loupGarou")
+    private DataSource ds;
+
+    /* pages d’erreurs */
+    private void invalidParameters(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/controleurErreur.jsp").forward(request, response);        
+    }
+
+    private void erreurBD(HttpServletRequest request,
+                HttpServletResponse response, DAOException e)
+            throws ServletException, IOException {
+        request.setAttribute("erreurMessage", e.getMessage());
+        request.getRequestDispatcher("/WEB-INF/bdErreur.jsp").forward(request, response);
+    }
+  
+    /**
+     * Actions possibles en GET : afficher (correspond à l’absence du param), getOuvrage.
+     */
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        actionAfficher(request, response);
+
+    }
+
+    /**
+     * 
+     * Affiche la page d’accueil avec la liste de tous les ouvrages. 
+     */
+    
+    private void actionAfficher(HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
+        
+        /* On interroge la base de données pour obtenir la liste des ouvrages */
+        /* On ajoute cette liste à la requête en tant qu’attribut afin de la transférer à la vue
+         * Rem. : ne pas confondre attribut (= objet ajouté à la requête par le programme
+         * avant un forward, comme ici)
+         * et paramètre (= chaîne représentant des données de formulaire envoyées par le client) */
+        /* Enfin on transfère la requête avec cet attribut supplémentaire vers la vue qui convient */
+        request.getRequestDispatcher("/index.html").forward(request, response);
+    }
+
+
+    
+    /**
+     * Actions possibles en POST : ajouter, supprimer, modifier.
+     * Une fois l’action demandée effectuée, on retourne à la page
+     * d’accueil avec actionAfficher(...)
+     */
+
+    public void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        actionAfficher(request, response);
+
+    }
+    
+    
+}
--- projetWEB/src/main/webapp/newPartie.jsp
+++ projetWEB/src/main/webapp/newPartie.jsp
@@ -1,17 +1,28 @@
-<%-- 
-    Document   : newPartie
-    Created on : Mar 20, 2017, 3:25:18 PM
-    Author     : nicolasl
---%>
-
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Nouvelle Partie</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Creation d'une nouvelle partie</h1>
+        <p> Veuillez saisir les parametres de la nouvelle partie </p>
+        <form method="post" action="setGame" accept-charset="UTF-8">
+        <ul>
+          <li> Nombre de participants : 
+            <SELECT name="joueurs" size="1">
+                <OPTION>5 - 10
+                <OPTION>10 - 15 
+                <OPTION>15 - 20 
+            </SELECT>
+          </li>
+          <li> Duree d'une journee : <input type="text" name="dayHour" value="14" size="2"/> h<input type="text" name="dayMin" value="00" size="2"/></li>
+          <li> Duree d'une nuit : <input type="text" name="nightHour" value="10" size="2"/> h <input type="text" name="nightMin" value="00" size="2"/></li>
+          <li> A quelle heure commencera la partie demain ? <input type="text" name="beginHour" value="8" size="2"/> h <input type="text" name="beginMinu" value="00" size="2"/></li>
+          <li> Probabilite d'attribution des pouvoirs speciaux :  <input type="text" name="powerNum" value="0" size="2"/>/<input type="text" name="powerDenum" value="0" size="2"/> </li>
+          <li> Proportion initiale des Loups-Garous :  <input type="text" name="werewolfNum" value="1" size="2"/>/<input type="text" name="werewolfDenum" value="3" size="2"/></li>
+        </ul>
+        <input type="submit" name="Creer une nouvelle partie" />
+      </form>
     </body>
 </html>
