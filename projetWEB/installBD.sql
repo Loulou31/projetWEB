63378ba6fbde2925c57decbe40ac053dd9869fc6
Claire Bagou <Claire.Bagou@ensimag.grenoble-inp.fr>
2017-03-24 17:41:35 +0100

    bd

--- projetWEB/installBD.sql
+++ projetWEB/installBD.sql
@@ -4,7 +4,6 @@
  */
 
 
-
 CREATE SEQUENCE id_seq ; 
 
 CREATE TABLE PARTIE (
@@ -18,7 +17,7 @@ CREATE TABLE PARTIE (
 ) ; 
 
 CREATE TABLE JOUEUR (
-    login varchar(10) NOT NULL, 
+    login varchar(10) NOT NULL PRIMARY KEY, 
     Rôle varchar(10) , 
     Statut int CONSTRAINT StatPos CHECK (Statut = 0 OR Statut = 1), 
     Pouvoir varchar(10),
@@ -31,6 +30,35 @@ CREATE TABLE MEMBRE (
     password varchar(10) NOT NULL
 );
 
+CREATE TABLE MESSAGE_SALLE_DISCUSSION (
+    login_expediteur varchar(10) NOT NULL, 
+    contenu varchar(100) NOT NULL, 
+    date_envoi date, 
+    PRIMARY KEY (login_expediteur, date_envoi), 
+    CONSTRAINT login_expediteurForeign FOREIGN KEY 
+        (login_expediteur) REFERENCES JOUEUR(login)
+);
+
+CREATE TABLE MESSAGE_REPAIRE (
+    login_expediteur varchar(10) NOT NULL, 
+    contenu varchar(100) NOT NULL, 
+    date_envoi date, 
+    PRIMARY KEY (login_expediteur, date_envoi), 
+    CONSTRAINT login_expediteurForeign2 FOREIGN KEY 
+        (login_expediteur) REFERENCES JOUEUR(login)
+);
+
+CREATE TABLE DECISION (
+    login_expeditaire varchar(10) NOT NULL, 
+    login_joueur_concerne varchar(10) NOT NULL, 
+    est_valide int CONSTRAINT EtatValide CHECK (est_valide = 0 OR est_valide = 1), 
+    date_envoi date,
+    CONSTRAINT login_joueurForeign FOREIGN KEY 
+        (login_joueur_concerne) REFERENCES JOUEUR(login), 
+    CONSTRAINT login_expForeign FOREIGN KEY 
+        (login_expeditaire) REFERENCES JOUEUR(login),
+    PRIMARY KEY(login_expeditaire, date_envoi)
+);
 
 INSERT INTO PARTIE (NbJoueurs, DureeJour, DureeNuit, HeureDebut, ProbaPouvoir, ProportionLG)
 VALUES (5, 1, 1, 8, 0.2, 0.5) ; 
@@ -38,5 +66,9 @@ VALUES (5, 1, 1, 8, 0.2, 0.5) ;
 INSERT INTO PARTIE (NbJoueurs, DureeJour, DureeNuit, HeureDebut, ProbaPouvoir, ProportionLG)
 VALUES (6, 1, 1, 8, 0.2, 0.5) ; 
 
- 
-SELECT * FROM MEMBRE;
+INSERT INTO DECISION VALUES ('bagouc', 0);
+
+SELECT * FROM MESSAGE_SALLE_DISCUSSION;
+SELECT * FROM MESSAGE_REPAIRE;
+SELECT * FROM DECISION; 
+
