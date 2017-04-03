898db9502a6ac327136d50a37328931af3e462a1
Cyril Gaunet <cyril.gaunet@ensimag.grenoble-inp.fr>
2017-04-03 15:32:17 +0200

    bagou la solo

--- projetWEB/src/main/java/dao/VillageoisDAO.java
+++ projetWEB/src/main/java/dao/VillageoisDAO.java
@@ -206,7 +206,7 @@ public class VillageoisDAO extends AbstractDatabaseDAO{
                 result.add(humain);
             }
         } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
+            throw new DAOException("Erreur BD heeeey" + e.getMessage(), e);
         }
         return result;
     }
