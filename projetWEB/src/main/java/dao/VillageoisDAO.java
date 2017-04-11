45b98d0939700b8c800557c3b68f8e9a17382a09
Louise Nicolas <louise.nicolas@ensimag.grenoble-inp.fr>
2017-04-11 14:46:20 +0200

    doc

--- projetWEB/src/main/java/dao/VillageoisDAO.java
+++ projetWEB/src/main/java/dao/VillageoisDAO.java
@@ -21,7 +21,172 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
         super(ds);
     }
 
-    //OK
+    
+
+    /**
+     * @brief Retourne un villageois à partie de son pseudo
+     * @param pseudo
+     * @return villageois
+     */
+    public Villageois getVillageois(String pseudo) {
+        Villageois villageois;
+        try (
+              Connection conn = getConn();) {
+            PreparedStatement st = conn.prepareStatement("SELECT * FROM JOUEUR WHERE login = ?");
+            st.setString(1, pseudo);
+            ResultSet rs = st.executeQuery();
+            System.out.println("RS : "+rs.next());
+            villageois = new Villageois(rs.getString("login"),
+                    rs.getInt("rolePartie"),
+                    rs.getInt("Statut"),
+                    rs.getString("Pouvoir"),
+                    rs.getInt("IdPartie"));
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD getVillageois" + e.getMessage(), e);
+        }
+        return villageois;
+    }
+
+    /**
+     * @brief Ajout d'un joueur dans une partie
+     * @param pseudo
+     * @param idPartie 
+     */
+    public void addPlayer(String pseudo, int idPartie) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("INSERT INTO JOUEUR VALUES  (?, -1, 1, ?, ?) ");
+            st.setString(1, pseudo);
+            st.setString(2, "rien");
+            st.setInt(3, idPartie);
+            st.executeUpdate();
+            st = conn.prepareStatement("Update Partie set nbJoueursVivants = nbJoueursVivants + 1 Where IdPartie = ?");
+            st.setInt(1, idPartie);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @brief Mise à jour du statut d'un villageois lorsqu'il meurt
+     * @param idPartie
+     * @param pseudo 
+     */
+    public void mortVillageois(int idPartie, String pseudo) {
+        try (Connection conn = getConn()) {
+            /* On passe le statut du villageois à mort = 0 */
+            PreparedStatement st = conn.prepareStatement("Update Joueur set Statut = 0 Where login = ?"); 
+            st.setString(1, pseudo);
+            st.executeUpdate();
+            /* On décrémente le nombre de joueurs vivants de la partie */
+            st = conn.prepareStatement("Update Partie set nbJoueursVivants = nbJoueursVivants - 1 Where IdPartie = ?");
+            st.setInt(1, idPartie);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @brief Supprime un villageois de la partie
+     * @param pseudo 
+     */
+    public void supprimerVillageois (String pseudo) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("DELETE FROM Joueur WHERE login = ?");
+            st.setString(1, pseudo);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD supp villageois " + e.getMessage(), e);
+        }
+    }
+    
+    /**
+     * @brief Supprime tous les villageois de la partie
+     * @param idPartie 
+     */
+    public void supprimerVillageois(int idPartie) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("DELETE FROM Joueur WHERE IdPartie = ?");
+            st.setInt(1, idPartie);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @brief Retourne nombre de joueurs dans la partie
+     * @param idPartie
+     * @return 
+     */
+    public int nombreJoueursPartie(int idPartie) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("SELECT count(*) FROM Joueur WHERE IdPartie = ?");
+            st.setInt(1, idPartie);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @brief Mise à jour rôle joueur
+     * @param pseudo
+     * @param role
+     */
+    public void updatePlayerRole(String pseudo, int role) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("UPDATE JOUEUR SET rolePartie = ?, pouvoir = 'rien' WHERE login = ?");
+            st.setInt(1, role);
+            st.setString(2, pseudo);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @brief Mise à jour statut joueur (vivant/mort)
+     * @param pseudo
+     * @param statut
+     */
+    public void updatePlayerStatus(String pseudo, int statut) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("UPDATE JOUEUR SET Statut = ? WHERE login = ?");
+            st.setInt(1, statut);
+            st.setString(2, pseudo);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @brief Mise à jour Pouvoir joueur
+     * @param pseudo
+     * @param pouvoir 
+     */
+    public void updatePlayerStatus(String pseudo, String pouvoir) {
+        try (Connection conn = getConn()) {
+            PreparedStatement st = conn.prepareStatement("UPDATE JOUEUR SET Pouvoir = ? WHERE login = ?");
+            st.setString(1, pouvoir);
+            st.setString(2, pseudo);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        }
+    }
+    
+    
+    
+    /**
+     * @brief Retourne la liste des villageois de la partie
+     * @param idPartie
+     * @return liste des villageois
+     */
     public List<Villageois> getListVillageois(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -43,7 +208,11 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
         return result;
     }
 
-    //ca me semble pas approprié... dans partie
+    /**
+     * @brief Accède à la liste des villageois vivants
+     * @param idPartie
+     * @return  liste des villageois vivants
+     */
     public List<Villageois> getListVillageoisVivants(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -65,7 +234,11 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
         return result;
     }
     
-    //same story
+    /**
+     * @brief Accède à la liste des villageois éliminés
+     * @param idPartie
+     * @return liste des villageois éliminés
+     */
     public List<Villageois> getListVillageoisMorts(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -86,136 +259,14 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
         }
         return result;
     }
-
-    //je dis oui
-    public Villageois getVillageois(String pseudo) {
-        Villageois villageois;
-        try (
-              Connection conn = getConn();) {
-            PreparedStatement st = conn.prepareStatement("SELECT * FROM JOUEUR WHERE login = ?");
-            st.setString(1, pseudo);
-            ResultSet rs = st.executeQuery();
-            System.out.println("RS : "+rs.next());
-            villageois = new Villageois(rs.getString("login"),
-                    rs.getInt("rolePartie"),
-                    rs.getInt("Statut"),
-                    rs.getString("Pouvoir"),
-                    rs.getInt("IdPartie"));
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD getVillageois" + e.getMessage(), e);
-        }
-        return villageois;
-    }
-
-    //excellent
-    public void addPlayer(String pseudo, int idPartie) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("INSERT INTO JOUEUR VALUES  (?, -1, 1, ?, ?) ");
-            st.setString(1, pseudo);
-            st.setString(2, "rien");
-            st.setInt(3, idPartie);
-            st.executeUpdate();
-            st = conn.prepareStatement("Update Partie set nbJoueursVivants = nbJoueursVivants + 1 Where IdPartie = ?");
-            st.setInt(1, idPartie);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
-
-    public void mortVillageois(int idPartie, String pseudo) {
-        try (Connection conn = getConn()) {
-            /* On passe le statut du villageois à mort = 0 */
-            PreparedStatement st = conn.prepareStatement("Update Joueur set Statut = 0 Where login = ?"); 
-            st.setString(1, pseudo);
-            st.executeUpdate();
-            /* On décrémente le nombre de joueurs vivants de la partie */
-            st = conn.prepareStatement("Update Partie set nbJoueursVivants = nbJoueursVivants - 1 Where IdPartie = ?");
-            st.setInt(1, idPartie);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
-
-    public void supprimerVillageois (String pseudo) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("DELETE FROM Joueur WHERE login = ?");
-            st.setString(1, pseudo);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD supp villageois " + e.getMessage(), e);
-        }
-    }
     
-        //suprime tous les villageois liés à une partie
-    public void supprimerVillageois(int idPartie) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("DELETE FROM Joueur WHERE IdPartie = ?");
-            st.setInt(1, idPartie);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
-
-    //bye bye dans partie
-    public int nombreJoueursPartie(int idPartie) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("SELECT count(*) FROM Joueur WHERE IdPartie = ?");
-            st.setInt(1, idPartie);
-            ResultSet rs = st.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
-
-    //on fait toutes les updates ici
-    /* Defini le role (humain ou LG) du joueur en début de partie */
-    public void updatePlayerRole(String pseudo, int role) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("UPDATE JOUEUR SET rolePartie = ?, pouvoir = 'rien' WHERE login = ?");
-            st.setInt(1, role);
-            st.setString(2, pseudo);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
-
-    /* Mise à jour du statut vivant ou mort du joueur */
-    public void updatePlayerStatus(String pseudo, int statut) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("UPDATE JOUEUR SET Statut = ? WHERE login = ?");
-            st.setInt(1, statut);
-            st.setString(2, pseudo);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
-
-    /* Mise à jour du pouvoir du joueur */
-    public void updatePlayerStatus(String pseudo, String pouvoir) {
-        try (Connection conn = getConn()) {
-            PreparedStatement st = conn.prepareStatement("UPDATE JOUEUR SET Pouvoir = ? WHERE login = ?");
-            st.setString(1, pouvoir);
-            st.setString(2, pseudo);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            throw new DAOException("Erreur BD " + e.getMessage(), e);
-        }
-    }
 
     /**
-     * donne la liste des villageois ne possedant pas de pouvoirs
+     *  @brief Accède à la liste des villageois ne possedant pas de pouvoirs
      *
      * @param idPartie
-     * @return
+     * @return liste des villageois sans pouvoirs
      */
-    //byebye dans partie
     public List<Villageois> getListVillageoisSansPouvoir(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -236,12 +287,11 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * donne la liste des humains ne possedant pas de pouvoirs
+     * @brief Accède à la liste des humains ne possedant pas de pouvoirs
      *
      * @param idPartie
-     * @return
+     * @return liste des villageois sans pouvoirs
      */
-    //same avant
     public List<Villageois> getListHumainsSansPouvoir(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -262,7 +312,11 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
         return result;
     }
 
-    //on a deja cette fonction en haut
+    /**
+     * @brief Accède à la liste des joueurs sans rôle attribué pour la partie
+     * @param idPartie
+     * @return liste des joueurs sans rôle
+     */
     public List<Villageois> getListJoueurs(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -283,12 +337,11 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * donne la liste des humains ne possedant pas de pouvoirs
+     * @brief Accède à la liste des loups ne possedant pas de pouvoirs
      *
      * @param idPartie
-     * @return
+     * @return liste des loups sans pouvoir
      */
-    //dans partie
     public List<Villageois> getListLoupsSansPouvoir(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -310,12 +363,11 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * donne la liste des humains morts
+     * @brief Accède à la liste des humains éliminés
      *
      * @param idPartie
-     * @return
+     * @return liste des humains éliminés
      */
-    //dans partie
     public List<Villageois> getListHumainsMorts(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
         try (Connection conn = getConn()) {
@@ -335,10 +387,10 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * donne liste humains vivants
+     * @brief Accéder à la liste des humains dans la partie
      *
      * @param idPartie
-     * @return
+     * @return lits des humains vivants
      */
     public List<Villageois> getListHumainsVivants(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
@@ -359,10 +411,10 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * renvoie la liste des humains de la partie
+     * @brief Accéder à la liste des humains d'une partie
      *
      * @param idPartie
-     * @return
+     * @return liste des humains
      */
     public List<Villageois> getListHumains(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
@@ -383,10 +435,10 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * renvoie la liste des loups de la partie
+     * @brief Accéder à la liste des loups-garou dans la partie
      *
      * @param idPartie
-     * @return
+     * @return liste des loups-garou
      */
     public List<Villageois> getListLoups(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
@@ -407,10 +459,10 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
     }
 
     /**
-     * donne liste loups vivants
+     * @brief Accéder à la liste des loups-garou vivants
      *
      * @param idPartie
-     * @return
+     * @return liste des loups vivants
      */
     public List<Villageois> getListLoupsVivants(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
@@ -430,11 +482,12 @@ public class VillageoisDAO extends AbstractDatabaseDAO {
         return result;
     }
 
+    
     /**
-     * donne la liste des loups morts
+     * @brief Accéder à la liste des loupe-garou éliminés
      *
      * @param idPartie
-     * @return
+     * @return liste des loups éliminés
      */
     public List<Villageois> getListLoupsMorts(int idPartie) {
         List<Villageois> result = new ArrayList<Villageois>();
